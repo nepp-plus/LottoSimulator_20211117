47a02037bbe6b90b5c2a96cfe5f16c126915547e
nepp-plus <nepp.plus@gmail.com>
2021-11-17 09:55:06 +0900

    함수 2개 생성 -> onCreate에서 사용  - 코드 정리 목적

--- app/src/main/java/com/neppplus/lottosimulator_20211117/MainActivity.kt
+++ app/src/main/java/com/neppplus/lottosimulator_20211117/MainActivity.kt
@@ -4,8 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+
+
+    }
+
 }
